acc50418336664ca34e7513c64efa6d3e78a3f15
pyshnenko <pyshnenko94@yandex.ru>
2025-06-20 09:10:54 +0300

    stop bot on server

--- back/src/app.ts
+++ back/src/app.ts
@@ -1,7 +1,6 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
-const { Telegraf } = require('telegraf');
-var jwt = require('jsonwebtoken');
+import { Telegraf, NarrowedContext } from 'telegraf';
 const bot = new Telegraf(String(process.env.TGTOK));
 import { session } from 'telegraf';
 import start from './bot/start';
@@ -13,22 +12,25 @@ import app from './api';
 bot.use(session());
 
 bot.telegram.setMyCommands([
-    { command: '/start', description: 'Старт' },
+    { command: '/start', description: 'Начинаем начинать' },
+    { command: '/support', description: 'Поддержка' },
+    { command: '/tariff', description: 'Денюшки' },
+    { command: '/info', description: 'Как пользоваться?' }
 ])
 
-bot.start(async (ctx: Context) => {
-    ctx.session = await start(ctx, ctx.session);
+bot.start(async (ctx: any) => {
+    ctx.session = await start(ctx as Context, (ctx as Context).session);
 });
 
-bot.on('callback_query', async (ctx: Context) => {
-    ctx.session = await callback(ctx, ctx.session);
+bot.on('callback_query', async (ctx: any) => {
+    ctx.session = await callback(ctx as Context, (ctx as Context).session);
 })
 
 bot.on('message', async (ctx: any) => {
-    ctx.session = await message(ctx, ctx.session);
+    ctx.session = await message((ctx as Context), (ctx as Context).session);
 })
 
-bot.launch();
+//bot.launch();
 
 bot.catch((err: any)=>console.log('Что-то с ботом' + String(err)));
 
